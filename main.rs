aeaa05df053ba5a8e6617cd67911dd3694aea139
Phillip Howard <phillipwh404@gmail.com>
2026-03-09 19:11:31 -0500

    Add files via upload
    This main.rs file is actually the combat for the "Fighting Fantasy: Game Book Series." 
    I wanted to learn rust and needed a project. While on a trip i realized i did not pack any dice that is required.

---
+++ main.rs
@@ -0,0 +1,71 @@
+use rand::Rng;
+use std::io;
+fn main() {
+// Variables that Change.
+let mut monster_name = String::new();
+let mut player_name = String::new();
+let mut input = String::new();
+
+// Inputs from player
+println!("enter thy name:");
+    io::stdin().read_line(&mut player_name).unwrap();
+    let player_name = player_name.trim().to_string();
+println!("enter the monster's name:");
+    io::stdin().read_line(&mut monster_name).unwrap();
+    let monster_name = monster_name.trim().to_string();
+println!("enter the monster's skill:");
+    io::stdin().read_line(&mut input).unwrap(); //input/output and taking the "mut input"
+    let monster_skill: i32 = input.trim().parse().unwrap(); // the input is assigned a name+number
+    input.clear(); //clear the input to then resume aka borrowing and then cleaning
+
+println!("enter the monster's stamina:");
+    io::stdin().read_line(&mut input).unwrap();
+    let mut monster_stamina: i32 = input.trim().parse().unwrap();
+    input.clear();
+
+println!("enter the player's skill:");
+    io::stdin().read_line(&mut input).unwrap();
+    let player_skill: i32 = input.trim().parse().unwrap();
+    input.clear();
+
+println!("enter the player's stamina:");
+    io::stdin().read_line(&mut input).unwrap();
+    let mut player_stamina: i32 = input.trim().parse().unwrap();
+    input.clear();
+
+// Preparing the Combat rounds
+let mut rng = rand::thread_rng();
+println!("The battle begins!");
+
+// While loops during combat rounds
+while monster_stamina > 0 && player_stamina > 0 {
+    let monster_roll = rng.gen_range(1..=12);
+    let player_roll = rng.gen_range(1..=12);
+    let monster_attack = monster_skill + monster_roll;
+    let player_attack = player_skill + player_roll;
+
+    println!("Monster's attack: {}", monster_attack);// to check code is correct
+    println!("Player's attack: {}", player_attack);// to check code is correct commit later
+
+    if player_attack > monster_attack {
+        monster_stamina -=2; //takes 2 damage
+        println!("{} endures injuries! Stamina: {}",monster_name, monster_stamina);
+                                        }
+    else if monster_attack > player_attack {
+        player_stamina -= 2;
+        println!("{} endures injuries!",player_name);
+                                            }
+    else {
+        println!("Clash! Neither injured!");
+        }
+    println!("-----------------");
+                                                }
+
+// If results from combat rounds
+if player_stamina > 0 {
+    println!("You breath out knowing, the {} is defeated!",monster_name);
+                        }
+else{
+    println!("Darkness encloses, and the sound of a friend speaking reaches your ears. Death greets you.");
+    }
+}
